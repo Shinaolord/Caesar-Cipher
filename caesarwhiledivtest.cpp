d7590888e15c079a9ad907089c59bca1db560f5b
Shinaolord <48556691+Shinaolord@users.noreply.github.com>
2019-03-15 08:49:32 -0400

    Fixed minor issues with outliers (x,y,z)

--- caesarwhiledivtest.cpp
+++ caesarwhiledivtest.cpp
@@ -13,6 +13,7 @@ std::string caesarencrypt(std::string ptext, int shift){
                 ctext+=' ';
             }
             else{
+                    if(shift<0){shift=shift+26;};
                     div_t division= div(ptext[i]+shift-65,26);
                     int rem= division.rem;
                     ctext += (char)(rem+65); /* take each letter, add the shift, subtract the value that makes it ASCII (65), take mod 26, then add the ASCII value again */
@@ -27,7 +28,7 @@ std::string caesarencrypt(std::string ptext, int shift){
 int main(){
     int shiftval;
     std::string ptxt;
-    std::cout<< "Please input the shift value, then the plaintext, seperated by a space"<<std::endl;
+    std::cout<< "Please input the shift value, then the plaintext, separated by a space"<<std::endl;
     while(std::cin>>shiftval && std::getline(std::cin,ptxt)){
         std::cout << "Plaintext:" << ptxt <<"\n";
         std::cout <<"Ciphertext:" << caesarencrypt(ptxt,shiftval)<<std::endl;
