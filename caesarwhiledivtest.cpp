78f92e78977885c4d4cfa42e4deb20d9632bdde8
Ankur <50777967+Ankur-Kumar-Gupta@users.noreply.github.com>
2020-10-01 03:05:12 +0530

    Update caesarwhiledivtest.cpp

--- caesarwhiledivtest.cpp
+++ caesarwhiledivtest.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
-#include<cstdlib>
+#include<conio.h>
+using namespace std;
 
-std::string caesarencrypt(std::string ptext, int shift){
-    std::string ctext= "";
-
-    for(int i=0;i<ptext.length();i++){
-
-            if(islower(ptext[i])){
-                ptext[i]=toupper(ptext[i]);
-            } /* turns everything into uppercase letters, which ASCII vary from 65 to 65+25=90 */
-            if(ptext[i]==' '){
-                ctext+=' ';
-            }
-            else{
-                    if(shift<0){shift=shift+26;};
-                    div_t division= div(ptext[i]+shift-65,26);
-                    int rem= division.rem;
-                    ctext += (char)(rem+65); /* take each letter, add the shift, subtract the value that makes it ASCII (65), take mod 26, then add the ASCII value again */
-            }
-
-
-
+string caesarencrypt(string ptext, int shift)
+{
+	string ctext= "";
+    for(int i=0;i<ptext.length();i++)
+	{
+		if(islower(ptext[i]))
+		{
+            ptext[i]=toupper(ptext[i]);		 /* turns everything into uppercase letters, which ASCII vary from 65 to 65+25=90 */
+        }
+        if(ptext[i]==' ')
+		{
+            ctext+=' ';
+        }
+    	else
+		{
+            if(shift<0)
+			{
+				shift=shift+26;
+			}
+			ctext += char(int( ptext [i] + shift - 65) % 26 + 65 );		/* take each letter, add the shift, subtract the value that makes it ASCII (65), take mod 26, then add the ASCII value again */
+        }
     }
     return ctext;
 }
-/* to decrypt, set shift= -shift, ie., if shift was 5 to encrypt, to decdrypt, set shift= -5*/
-int main(){
+int main()
+{
     int shiftval;
-    std::string ptxt;
-    std::cout<< "Please input the shift value, then the plaintext, separated by a space"<<std::endl;
-    while(std::cin>>shiftval && std::getline(std::cin,ptxt)){
-        std::cout << "Plaintext:" << ptxt <<"\n";
-        std::cout <<"Ciphertext:" << caesarencrypt(ptxt,shiftval)<<std::endl;
-    }
-
+    string ptxt;
+    cout<< "Please input the shift value, then the message, separated by a space ..."<<endl<<"(to decrypt, set shift with (-)ve sign)"<<endl<<"Type Here : "<<endl;
+	cin>>shiftval;
+	getline(cin,ptxt);			/* to decrypt, set shift= -shift, ie., if shift was 5 to encrypt, to decrypt, set shift= -5*/
+    cout << "Plaintext:" << ptxt <<"\n";
+    cout <<"Ciphertext:" << caesarencrypt(ptxt,shiftval);
+    getch();
     return 0;
 }
